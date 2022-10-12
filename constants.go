b16dd35b2f36b73cfce99880539f76ba7ce0a7c4
sophiewanjiku <98793987+sophiewanjiku@users.noreply.github.com>
2022-10-12 06:25:47 +0300

    constants in go

--- constants.go
+++ constants.go
@@ -1,8 +1,39 @@
 package main
-import(
+
+import (
 	"fmt"
 )
 
-func main(){
-	
+/*
+1. naming of constants. -> named the same way variables are named.
+typed contsants. include the data type of the constants
+the value of a consant cannot be modified. constants must be equal to something executed at compietime and not run time.
+*/
+/*
+2.  enumerated constants. declared at package level.
+constanst can be declared in block code
+example:
+*/
+const (
+	i = iota
+	j
+	k
+)
+
+func main() {
+	//typed contsants. include the data type of the constants
+	const myConst int = 42
+	fmt.Printf("%v, %T\n", myConst, myConst)
+
+	// untyped constants
+	const a = 27
+	fmt.Printf("%v, %T\n", a, a)
+
+	// arithmetic operations on constants
+	fmt.Printf("%v, %T\n", myConst+a, myConst+a)
+
+	//printing enumerated constants
+	fmt.Printf("%v\n", i)
+	fmt.Printf("%v\n", j)
+	fmt.Printf("%v\n", k)
 }
---
+++ constants2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+// a simple application to show the use of enumerated constants.
+const(
+	catSpecialist = iota
+	dogSpecialist
+	cowSpecialist
+)
+
+
+func main(){
+	var animalSpecialist = 
+	fmt .Printf(" ")
+
+}
