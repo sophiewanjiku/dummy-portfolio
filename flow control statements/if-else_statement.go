e442256fd6153d84e7b0d8f4d33b1e8bb66a2170
sophiewanjiku <98793987+sophiewanjiku@users.noreply.github.com>
2022-10-26 16:08:35 +0300

    add comment

--- flow control statements/if-else_statement.go
+++ flow control statements/if-else_statement.go
@@ -5,12 +5,13 @@ import (
 )
 
 func main() {
-	// if_else statement to determine the room temperature of a certain day
+	// if_else statement to determine the room temperature of a certain day.
+	//the else statement has to be on the same line as the closing calibracket for the if statement. if not, you'll receive an error.
 	roomTemp := 27
-	if (roomTemp > 25) {
+	if roomTemp > 25 {
 		fmt.Println("It is so hot today!")
 	} else {
-		fmt.Println ("it is cool today")
+		fmt.Println("it is cool today")
 	}
 	fmt.Println()
 }
